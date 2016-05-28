6b33310b5e634d6c495d004221aa0a488a721de8
Stephen Hurd <stephen.hurd@broadcom.com>
2016-05-27 18:07:47 -0700

    Initial framework for bnxt iflib driver
    All optional items are removed, and all functions are merely stubs
    which fail or do nothing.

---
+++ sys/dev/bnxt/bnxt.h
@@ -0,0 +1,51 @@
+/*-
+ * Broadcom NetXtreme-C/E network driver.
+ *
+ * Copyright (c) 2016 Broadcom, All Rights Reserved.
+ * The term Broadcom refers to Broadcom Limited and/or its subsidiaries
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS'
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#ifndef _BNXT_H
+#define _BNXT_H
+
+/* PCI IDs */
+#define BROADCOM_VENDOR_ID	0x14E4
+
+#define BCM57301	0x16C8
+#define BCM57302	0x16C9
+#define BCM57304	0x16CA
+#define BCM57402	0x16D0
+#define BCM57404	0x16D1
+#define BCM57406	0x16D2
+
+#define BNXT_TSO_SIZE		65536
+
+struct bnxt_softc {
+};
+
+#endif /* _BNXT_H */
---
+++ sys/dev/bnxt/bnxt_txrx.c
@@ -0,0 +1,116 @@
+/*-
+ * Broadcom NetXtreme-C/E network driver.
+ *
+ * Copyright (c) 2016 Broadcom, All Rights Reserved.
+ * The term Broadcom refers to Broadcom Limited and/or its subsidiaries
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS'
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <net/if_var.h>
+#include <net/ethernet.h>
+#include <net/iflib.h>
+
+#include "opt_inet.h"
+#include "opt_inet6.h"
+#include "opt_rss.h"
+
+#include "bnxt.h"
+
+/*
+ * Function prototypes
+ */
+
+static int bnxt_isc_txd_encap(void *arg, if_pkt_info_t pi);
+static void bnxt_isc_txd_flush(void *arg, uint16_t txqid, uint32_t pidx);
+static int bnxt_isc_txd_credits_update(void *arg, uint16_t txqid, uint32_t cidx,
+    bool clear);
+
+static void bnxt_isc_rxd_refill(void *arg, uint16_t rxqid, uint8_t flid,
+    uint32_t pidx, uint64_t *paddrs, caddr_t *vaddrs, uint16_t count);
+static void bnxt_isc_rxd_flush(void *arg, uint16_t rxqid, uint8_t flid,
+    uint32_t pidx);
+static int bnxt_isc_rxd_available(void *arg, uint16_t rxqid, uint32_t idx);
+static int bnxt_isc_rxd_pkt_get(void *arg, if_rxd_info_t ri);
+
+extern void bnxt_if_enable_intr(if_ctx_t ctx);
+extern int bnxt_intr(void *arg);
+
+struct if_txrx bnxt_txrx  = {
+	bnxt_isc_txd_encap,
+	bnxt_isc_txd_flush,
+	bnxt_isc_txd_credits_update,
+	bnxt_isc_rxd_available,
+	bnxt_isc_rxd_pkt_get,
+	bnxt_isc_rxd_refill,
+	bnxt_isc_rxd_flush,
+	bnxt_intr
+};
+
+/*
+ * Device Dependent Packet Transmit and Receive Functions
+ */
+
+static int bnxt_isc_txd_encap(void *arg, if_pkt_info_t pi)
+{
+	return ENOSYS;
+}
+
+static void bnxt_isc_txd_flush(void *arg, uint16_t txqid, uint32_t pidx)
+{
+	return;
+}
+
+static int bnxt_isc_txd_credits_update(void *arg, uint16_t txqid, uint32_t cidx, bool clear)
+{
+	return 0;	// No error return?
+}
+
+static void bnxt_isc_rxd_refill(void *arg, uint16_t rxqid, uint8_t flid,
+				uint32_t pidx, uint64_t *paddrs,
+				caddr_t *vaddrs, uint16_t count)
+{
+	return;
+}
+
+static void bnxt_isc_rxd_flush(void *arg, uint16_t rxqid, uint8_t flid,
+    uint32_t pidx)
+{
+	return;
+}
+
+static int bnxt_isc_rxd_available(void *arg, uint16_t rxqid, uint32_t idx)
+{
+	return 0;	// No error return?
+}
+
+static int bnxt_isc_rxd_pkt_get(void *arg, if_rxd_info_t ri)
+{
+	return ENOSYS;
+}
