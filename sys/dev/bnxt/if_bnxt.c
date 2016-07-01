Broadcom/freebsd-nxt

commit 1a801546b7284483fbfa9c51a79429f7e23b134e

Fix invalid TX BDs, handle errors better
Long TSO BDs were getting random garbage in their flags_type value due to the
length hint lookup table not being long enough.  Stop using the table for sizes
over 2048 bytes.

Check the completion type of TX completions, and log errors

If we are using an offload (including TSO), we will use two TX BDs for the
first frag and one for each after that.  Since we can use a max of 32 BDs, this
makes our effective segment limit 31, not 32.

Use the second TX ring for the completion queue for now since the first one
appears to be the only one getting bus DMA sync calls.

sys/dev/bnxt/if_bnxt.c
@@ -293,8 +293,9 @@ bnxt_tx_queues_alloc(if_ctx_t ctx, caddr_t *vaddrs,
 		    softc->tx_cp_rings[i].ring.id * 0x80;
 		softc->tx_cp_rings[i].ring.ring_size =
 		    softc->scctx->isc_ntxd * 2;
-		softc->tx_cp_rings[i].ring.vaddr = vaddrs[i * ntxqs];
-		softc->tx_cp_rings[i].ring.paddr = paddrs[i * ntxqs];
+		/* The second one never gets a bus DMA sync... use it for cp */
+		softc->tx_cp_rings[i].ring.vaddr = vaddrs[i * ntxqs + 1];
+		softc->tx_cp_rings[i].ring.paddr = paddrs[i * ntxqs + 1];
 
 		/* Set up the TX ring */
 		softc->tx_rings[i].phys_id = (uint16_t)HWRM_NA_SIGNATURE;
@@ -303,8 +304,8 @@ bnxt_tx_queues_alloc(if_ctx_t ctx, caddr_t *vaddrs,
 		    (softc->scctx->isc_nrxqsets * 2) + 1 + i;
 		softc->tx_rings[i].doorbell = softc->tx_rings[i].id * 0x80;
 		softc->tx_rings[i].ring_size = softc->scctx->isc_ntxd;
-		softc->tx_rings[i].vaddr = vaddrs[i * ntxqs + 1];
-		softc->tx_rings[i].paddr = paddrs[i * ntxqs + 1];
+		softc->tx_rings[i].vaddr = vaddrs[i * ntxqs];
+		softc->tx_rings[i].paddr = paddrs[i * ntxqs];
 
 		bnxt_create_tx_sysctls(softc, i);
 	}
@@ -571,8 +572,8 @@ bnxt_attach_pre(if_ctx_t ctx)
 		goto failed;
 
 	/* Now set up iflib sc */
-	scctx->isc_tx_nsegments = 32,
-	scctx->isc_tx_tso_segments_max = 32;
+	scctx->isc_tx_nsegments = 31,
+	scctx->isc_tx_tso_segments_max = 31;
 	scctx->isc_tx_tso_size_max = BNXT_TSO_SIZE;
 	scctx->isc_tx_tso_segsize_max = BNXT_TSO_SIZE;
 	scctx->isc_vectors = softc->pf.max_cp_rings;

sys/dev/bnxt/bnxt_txrx.c
@@ -84,20 +84,6 @@ static const uint16_t bnxt_tx_lhint[] = {
 	TX_BD_SHORT_FLAGS_LHINT_LT2K,
 	TX_BD_SHORT_FLAGS_LHINT_LT2K,
 	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
-	TX_BD_SHORT_FLAGS_LHINT_GTE2K,
 };
 
 static int
@@ -125,8 +111,12 @@ bnxt_isc_txd_encap(void *sc, if_pkt_info_t pi)
 	    pi->ipi_new_pidx);
 	tbd->len = htole16(pi->ipi_segs[seg].ds_len);
 	tbd->addr = htole64(pi->ipi_segs[seg++].ds_addr);
-	flags_type = (pi->ipi_nsegs + need_hi) << TX_BD_SHORT_FLAGS_BD_CNT_SFT;
-	flags_type |= bnxt_tx_lhint[pi->ipi_len >> 9];
+	flags_type = ((pi->ipi_nsegs + need_hi) <<
+	    TX_BD_SHORT_FLAGS_BD_CNT_SFT) & TX_BD_SHORT_FLAGS_BD_CNT_MASK;
+	if (pi->ipi_len >= 2048)
+		flags_type |= TX_BD_SHORT_FLAGS_LHINT_GTE2K;
+	else
+		flags_type |= bnxt_tx_lhint[pi->ipi_len >> 9];
 
 	if (need_hi) {
 		flags_type |= TX_BD_LONG_TYPE_TX_BD_LONG;
@@ -202,6 +192,8 @@ bnxt_isc_txd_credits_update(void *sc, uint16_t txqid, uint32_t idx, bool clear)
 	bool v_bit = cpr->v_bit;
 	bool last_v_bit;
 	uint32_t last_cons;
+	uint16_t type;
+	uint16_t err;
 
 	for (;;) {
 		last_cons = cons;
@@ -210,10 +202,31 @@ bnxt_isc_txd_credits_update(void *sc, uint16_t txqid, uint32_t idx, bool clear)
 		__builtin_prefetch(&cmpl[RING_NEXT(&cpr->ring, cons)]);
 
 		if (!CMP_VALID(&cmpl[cons], v_bit))
-			break;
+			goto cmpl_invalid;
 
-		avail += le32toh(cmpl[cons].opaque) >> 24;
+		type = cmpl[cons].flags_type & TX_CMPL_TYPE_MASK;
+		switch (type) {
+		case TX_CMPL_TYPE_TX_L2:
+			err = (le16toh(cmpl[cons].errors_v) &
+			    TX_CMPL_ERRORS_BUFFER_ERROR_MASK) >>
+			    TX_CMPL_ERRORS_BUFFER_ERROR_SFT;
+			if (err)
+				device_printf(softc->dev,
+				    "TX completion error %u\n", err);
+			avail += le32toh(cmpl[cons].opaque) >> 24;
+			break;
+		default:
+			if (type & 1) {
+				NEXT_CP_CONS_V(&cpr->ring, cons, v_bit);
+				if (!CMP_VALID(&cmpl[cons], v_bit))
+					goto cmpl_invalid;
+			}
+			device_printf(softc->dev,
+			    "Unhandled TX completion type %u\n", type);
+			break;
+		}
 	}
+cmpl_invalid:
 
 	if (clear && avail) {
 		cpr->cons = last_cons;

commit 1bd0c33b226d3e925015acf45843962a7084f0d1

Fix assertion introduced by 1a80154
Don't use the second queue for the completion ring or the mbuf tracking
will go nuts.  This should be fixed with the CQ API changes.

sys/dev/bnxt/if_bnxt.c
@@ -293,9 +293,8 @@ bnxt_tx_queues_alloc(if_ctx_t ctx, caddr_t *vaddrs,
 		    softc->tx_cp_rings[i].ring.id * 0x80;
 		softc->tx_cp_rings[i].ring.ring_size =
 		    softc->scctx->isc_ntxd * 2;
-		/* The second one never gets a bus DMA sync... use it for cp */
-		softc->tx_cp_rings[i].ring.vaddr = vaddrs[i * ntxqs + 1];
-		softc->tx_cp_rings[i].ring.paddr = paddrs[i * ntxqs + 1];
+		softc->tx_cp_rings[i].ring.vaddr = vaddrs[i * ntxqs];
+		softc->tx_cp_rings[i].ring.paddr = paddrs[i * ntxqs];
 
 		/* Set up the TX ring */
 		softc->tx_rings[i].phys_id = (uint16_t)HWRM_NA_SIGNATURE;
@@ -304,8 +303,8 @@ bnxt_tx_queues_alloc(if_ctx_t ctx, caddr_t *vaddrs,
 		    (softc->scctx->isc_nrxqsets * 2) + 1 + i;
 		softc->tx_rings[i].doorbell = softc->tx_rings[i].id * 0x80;
 		softc->tx_rings[i].ring_size = softc->scctx->isc_ntxd;
-		softc->tx_rings[i].vaddr = vaddrs[i * ntxqs];
-		softc->tx_rings[i].paddr = paddrs[i * ntxqs];
+		softc->tx_rings[i].vaddr = vaddrs[i * ntxqs + 1];
+		softc->tx_rings[i].paddr = paddrs[i * ntxqs + 1];
 
 		bnxt_create_tx_sysctls(softc, i);
 	}